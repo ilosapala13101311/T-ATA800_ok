bad8a6508f9c999b382663f8e909cf1ddfd9c0e3
ilosapala13101311 <ilosapala@gmail.com>
2026-06-23 23:06:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ function showNextMessage(){
   msg.style.padding = "10px 25px";
   msg.style.borderRadius = "20px";
   msg.style.maxWidth = "90vw";
-  msg.style.minWidth = "260px";
+  msg.style.miniWidth = "260px";
   msg.style.whiteSpace = "nowrap";
 
   msg.style.fontSize = "20px";
@@ -357,302 +357,39 @@ function showConfetti(){
 
   const icons = ["🎊","🎉","⭐","🏆","👑"];
 
+  for(let i=0;i<30;i++){
   for(let i=0;i<15;i++){
 
     const conf = document.createElement("div");
 
-    conf.innerHTML =
-      icons[Math.floor(Math.random()*icons.length)];
-
-    conf.style.position = "fixed";
-    conf.style.left = Math.random()*100 + "%";
-    conf.style.top = "-50px";
-
-    conf.style.fontSize =
-      (20 + Math.random()*25) + "px";
-
-    conf.style.zIndex = "9999";
-
-    conf.style.transition = "all 3s linear";
-
-    document.body.appendChild(conf);
-
-    setTimeout(()=>{
-
-      conf.style.transform =
-        `translateY(${window.innerHeight+100}px)
-         rotate(${Math.random()*720}deg)`;
-
-    },50);
-
-    setTimeout(()=>{
-      conf.remove();
-    },3000);
-
-  }
-
-}
-function endGame(){
-
-  gameEnded = true;
-  messageQueue.length = 0;
-
-  const koniec = document.createElement("div");
-
-  koniec.id = "koniecMisji";
-
-  koniec.innerHTML = "UPS! KONIEC GRY";
-
-  koniec.style.position = "fixed";
-  koniec.style.inset = "0";
-  koniec.style.background = "rgba(0,0,0,0.95)";
-  koniec.style.color = "white";
-  koniec.style.display = "flex";
-  koniec.style.justifyContent = "center";
-  koniec.style.alignItems = "center";
-  koniec.style.fontSize = "35px";
-  koniec.style.fontWeight = "bold";
-  koniec.style.zIndex = "99999";
-
-  document.body.appendChild(koniec);
-endSound.currentTime = 0;
-endSound.play().catch(()=>{});
-
-  bgMusic.pause();
-
-  let procent = 75;
-
-  if(score >= 1500) procent = 88;
-  if(score >= 3000) procent = 95;
-  if(score >= 5000) procent = 100;
-
-  setTimeout(()=>{
-
-    koniec.remove();
-
-    document.getElementById("gameOver").style.display="flex";
-kctataSound.currentTime = 0;
-kctataSound.play().catch(()=>{});
-
-    document.getElementById("finalScore").innerHTML =
-    `
-    👑 CERTYFIKAT TATY ROKU<br><br>
-    Wynik: ${procent}%<br><br>
-    ❤️ Kochamy Cię Tatusiu ❤️<br><br>
-    Marysia • Jaś
-    `;
-
-  },3000);
-
-}
-function loop(){
-  if(gameEnded) return;
-
-  player.style.left=playerX+"px";
-
-  for(let i=items.length-1;i>=0;i--){
-
-    let item=items[i];
-
-    let speedMultiplier = 0.7; // początek wolniej
-
-if(score >= 10000){
-  speedMultiplier = 2.2;
-}
-else if(score >= 8000){
-  speedMultiplier = 1.8;
-}
-else if(score >= 5000){
-  speedMultiplier = 1.3;
-}
-else if(score >= 3000){
-  speedMultiplier = 1;
+@ -470,7 +470,8 @@ else if(score >= 3000){
 }
 
 item.y += item.speed * speedMultiplier;
+    item.el.style.top=item.y+"px";
     item.el.style.transform =
 `translate(${item.x}px, ${item.y}px)`;
 
     let dx=Math.abs(item.x-playerX);
 
-    if(dx<55 && item.y>window.innerHeight-100){
-
-      score += item.type.points;
-if(score >= 10000 && !hardModeShown){
-  hardModeShown = true;
-  showLevelMessage("🔥 TRYB HARD!");
-}
-
-   if(item.type.family){
-
-  humor = Math.min(100, humor + 10);
-
- if(item.type.emoji === "👧"){
-
-  lastMarysia = Date.now();
-
-  const isCombo = (Date.now() - lastJas < 3000);
-
-  if(!isCombo){
-    marysiaSound.currentTime = 0;
-    marysiaSound.play().catch(()=>{});
-  }
-
-  if(Math.random() < 0.20){
-    showLevelMessage("😘 Marysia daje buziaka!");
-    showHearts();
-  }
-
-  if(Date.now() - lastJas < 3000){
-
-    score += 500;
-
-    showLevelMessage("❤️ RODZINNE KOMBO! +500");
-    showHearts();
-
-    marysiaSound.pause();
-    przytulasSound.pause();
-
-    marysiaSound.currentTime = 0;
-    przytulasSound.currentTime = 0;
-
-    showComboPhoto();
-  }
-}
-
-  if(item.type.emoji === "👦"){
-
-  lastJas = Date.now();
-
-  const isCombo = (Date.now() - lastMarysia < 3000);
-
- if(!isCombo){
-  przytulasSound.currentTime = 0;
-  przytulasSound.play().catch(()=>{});
-}
-
-    if(Math.random() < 0.20){
-      showLevelMessage("🤗 Jaś przytula Tatę!");
-      showHearts();
-    }
-
-    if(Date.now() - lastMarysia < 3000){
-      score += 500;
-      showLevelMessage("❤️ RODZINNE KOMBO! +500");
-showHearts();
-
-marysiaSound.pause();
-  przytulasSound.pause();
-
-  marysiaSound.currentTime = 0;
-  przytulasSound.currentTime = 0;
-  showComboPhoto();
-}
-}
-
-      if(item.type.emoji==="🧦"){
-        humor=Math.max(0,humor-10);
-smrodekSound.currentTime = 0;
-  smrodekSound.play().catch(()=>{});
-        showLevelMessage("🧦 Śmierdząca sprawa!");
-      }
-if(item.type.emoji==="🥤"){
-        humor=Math.max(0,humor+10);
-helenaSound.currentTime = 0;
-  helenaSound.play().catch(()=>{});
-        showLevelMessage("🥤 Wincyj cukru!");
-      }
-      if(item.type.emoji==="🍕"){
-  humor = 100;
-mniamSound.currentTime = 0;
-  mniamSound.play().catch(()=>{});
-  showLevelMessage("🍕 Capriciosa na grubym, raz!");
-showPizzaPhoto();
-}
-if(item.type.emoji==="🏋️"){
-  humor = Math.min(100, humor + 5);
-besttataSound.currentTime = 0;
-  besttataSound.play().catch(()=>{});
-
-  showLevelMessage("💪 TRENING ZALICZONY!");
-showGymPhoto();
-}
-       if(item.type.emoji==="🎮"){
-  humor = 100;
-graSound.currentTime = 0;
-  graSound.play().catch(()=>{});
-
-  showLevelMessage("🎮 ZOG!");
-showGamePhoto();
-       }
-
-if(item.type.emoji==="💩"){
-  humor=Math.max(0,humor-15);
-kupaSound.currentTime = 0;
-  kupaSound.play().catch(()=>{});
-  showLevelMessage("💩 WDEPNIĘTE!");
-}
-if(item.type.emoji==="🚬"){
-  humor=Math.max(0,humor-25);
+@ -598,6 +599,11 @@ if(item.type.emoji==="🚬"){
 
   fujkaSound.currentTime = 0;
   fujkaSound.play().catch(()=>{});
+}
+if(sound.paused){
+   sound.currentTime = 0;
+   sound.play();
+}
 
   showLevelMessage("🚬 FUJ! TATA NIE PAL!");
 }
-      updateHUD();
-
-      item.el.remove();
-      items.splice(i,1);
-    }
-
-    else if(item.y>window.innerHeight){
-
-      if(item.type.family){
-        lives--;
-
-        if(lives<=0){
-          endGame();
-          return;
-        }
-      }
-
-      item.el.remove();
-      items.splice(i,1);
-      updateHUD();
-    }
-  }
-
+@ -627,7 +633,7 @@ if(item.type.emoji==="🚬"){
   requestAnimationFrame(loop);
 }
 
+setInterval(spawnItem,1000);
 setInterval(spawnItem,1500);
 
 document.getElementById("playBtn")
 .addEventListener("click",()=>{
-
-  bgMusic.play();
-
-[
-  marysiaSound,
-  przytulasSound,
-  comboSound,
-  fujkaSound,
-  kupaSound,
-  mniamSound,
-  graSound,
-  helenaSound,
-  besttataSound,
-  kctataSound,
-  endSound
-].forEach(sound=>{
-  sound.load();
-});
-
-  document.getElementById("intro")
-  .style.display="none";
-
-  updateHUD();
-  loop();
-
-});
